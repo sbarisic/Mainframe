8516f1f272c381239fafc6a45bfd797323495323
Saša Barišić <sbarisic@vub.hr>
2025-11-29 23:31:24 +0100

    Render loop

---
+++ Mainframe/Mainframe.Client/wwwroot/scr.js
@@ -0,0 +1,9 @@
+﻿function gameLoop(timeStamp) {
+	window.requestAnimationFrame(gameLoop);
+	theInstance.invokeMethodAsync('GameLoop', timeStamp);
+}
+
+window.initGame = (instance) => {
+	window.theInstance = instance;
+	window.requestAnimationFrame(gameLoop);
+};
